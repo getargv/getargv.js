0d8b1dd8dc1ad40a4efd195d63d18016c2142209
Camden Narzt <c.narzt@me.com>
2025-03-29 13:42:35 -0600

    DRY arg validation

--- lib/binding.ts
+++ lib/binding.ts
@@ -1,34 +1,31 @@
-import { TextEncoding, TextDecoderOptions, GetArgv } from './types';
+import type { TextEncoding, TextDecoderOptions, GetArgv } from './types';
 
 const addon: GetArgv = require('../build/Release/getargv_native.node');
 
-addon.as_string = function as_string(pid: number, encoding: TextEncoding, nuls?: boolean, skip?: number, options?: TextDecoderOptions): string {
-    switch (arguments.length) {
-        case 0: throw new TypeError('The "pid" argument must be specified');
-        case 1: throw new TypeError('The "encoding" argument must be specified');
-        case 2: case 3: case 4: case 5: break;
-        default: throw new TypeError('Too many arguments were specified');
-    }
+type ArgKinds = Parameters<GetArgv['as_string']> | Parameters<GetArgv['as_array']>;
+
+function validate_args<This, Args extends ArgKinds, Return>(target: (this: This, ...args: Args) => Return) {
+  return function(this: This, ...args: Args): Return {
+    if (arguments.length == 0) throw new TypeError('The "pid" argument must be specified');
+    if (arguments.length == 1) throw new TypeError('The "encoding" argument must be specified');
+    const [ pid, encoding ] = arguments;
     if (typeof pid !== "number") { throw new TypeError('The "pid" argument must be a number') }
     if (typeof encoding !== "string") { throw new TypeError(`The "encoding" argument must be a string, is a: ${typeof encoding}`) }
-    const decoder = new TextDecoder(encoding, options);
-    const array = addon.get_argv_of_pid(pid, nuls ?? false, skip ?? 0);
-    return decoder.decode(array);
+    return target.apply(this, args);
+  }
 }
 
-addon.as_array = function as_array(pid: number, encoding: TextEncoding, options: TextDecoderOptions): Array<string> {
-    switch (arguments.length) {
-        case 0: throw new TypeError('The "pid" argument must be specified');
-        case 1: throw new TypeError('The "encoding" argument must be specified');
-        case 2: case 3: break;
-        default: throw new TypeError('Too many arguments were specified');
-    }
-    if (typeof pid !== "number") { throw new TypeError('The "pid" argument must be a number') }
-    if (typeof encoding !== "string") { throw new TypeError(`The "encoding" argument must be a string, is a: ${typeof encoding}`) }
-    const decoder = new TextDecoder(encoding, options);
-    const array = addon.get_argv_and_argc_of_pid(pid);
-    return array.map(b => decoder.decode(b));
-}
+addon.as_string = validate_args(function (pid: number, encoding: TextEncoding, nuls: boolean = false, skip: number = 0, options?: TextDecoderOptions): string {
+  const decoder = new TextDecoder(encoding, options);
+  const array = addon.get_argv_of_pid(pid, nuls, skip);
+  return decoder.decode(array);
+});
+
+addon.as_array = validate_args(function (pid: number, encoding: TextEncoding, options?: TextDecoderOptions): Array<string> {
+  const decoder = new TextDecoder(encoding, options);
+  const array = addon.get_argv_and_argc_of_pid(pid);
+  return array.map(b => decoder.decode(b));
+});
 
 exports.PID_MAX = addon.PID_MAX;
 exports.ARG_MAX = addon.ARG_MAX;
