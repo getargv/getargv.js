bd2e215964760af40bcae9b904d0a085919d2874
Camden Narzt <c.narzt@me.com>
2025-05-12 09:09:35 +0200

    update bun tests

--- test/bun_test.ts
+++ test/bun_test.ts
@@ -1,42 +1,26 @@
-import { describe, test, expect } from 'bun:test';
+import { describe, test as it, expect } from 'bun:test';
 import Getargv from "../dist/binding";
-import process from "process";
-
-const [major, _minor, _patch] = process.versions.node.split('.').map(Number);
-
-const removePrefix = (value: string, prefix: string) => value.startsWith(prefix) ? value.slice(prefix.length) : value;
-
-function fixupPerNodeVersion(input: string) {
-  if (major && major <= 20) return input;
-  return removePrefix(input, process.cwd() + "/");
-}
-
-function expected_args() {
-  const enc = new TextEncoder();
-  return [process.argv0, ...process.execArgv, ...process.argv.slice(1)].map(a =>
-    enc.encode(fixupPerNodeVersion(a) + "\0").buffer
-  );
-};
+import { makeChild, expectedArgs, pid, toBuffer, fromBuffer, expectedString, expectedArray, appendBuffers } from "./helper.js";
 
 describe("Testing constants", () => {
 
-  test('Exports something', () =>
+  it('Exports something', () =>
     expect(Getargv).toBeDefined()
   );
 
-  test('Exports PID_MAX constant', () =>
+  it('Exports PID_MAX constant', () =>
     expect(Getargv).toHaveProperty('PID_MAX')
   );
 
-  test('PID_MAX constant is correct', () =>
+  it('PID_MAX constant is correct', () =>
     expect(Getargv.PID_MAX).toBe(99999)
   );
 
-  test('exports ARG_MAX constant', () =>
+  it('exports ARG_MAX constant', () =>
     expect(Getargv).toHaveProperty('ARG_MAX')
   );
 
-  test('ARG_MAX is correct', () =>
+  it('ARG_MAX is correct', () =>
     expect(Getargv.ARG_MAX).toBe(1024 * 1024)
   );
 
@@ -44,126 +28,136 @@ describe("Testing constants", () => {
 
 describe("Getargv.as_string errors", () => {
 
-  test('Requires the "pid" argument be specified', () =>
+  it('Requires the "pid" argument be specified', () =>
     expect(Getargv.as_string).toThrow({
       name: 'TypeError',
       message: 'The "pid" argument must be specified',
     })
   );
 
-  test('Requires the "encoding" argument be specified', () =>
-    expect(() => Getargv.as_string(process.pid)).toThrow({
+  it('Requires the "encoding" argument be specified', () =>
+    expect(() => Getargv.as_string(pid)).toThrow({
       name: 'TypeError',
       message: 'The "encoding" argument must be specified',
     })
   );
 
-  test('Requires the "pid" argument be a number', () =>
+  it('Requires the "pid" argument be a number', () =>
     expect(() => Getargv.as_string('a', 'utf-8')).toThrow({
       name: 'TypeError',
       message: 'The "pid" argument must be a number',
     })
   );
 
-  test('Requires the "encoding" argument be valid', () =>
-    expect(() => Getargv.as_string(process.pid, 'a')).toThrow({
+  it('Requires the "encoding" argument be valid', () =>
+    expect(() => Getargv.as_string(pid, 'a')).toThrow({
       name: 'RangeError',
       message: 'Unsupported encoding label "a"',
     })
   );
 
-  test('Throws if the arguments cannot be encoded as requested', () =>
-    expect(() => Getargv.as_string(process.pid, "utf-16", undefined, undefined, { fatal: true })).toThrow({
-      name: 'TypeError',
-      message: "The encoded data was not valid UTF-16LE data",
-    })
-  );
+  it('Throws if the arguments cannot be encoded as requested', () => {
+    const child = makeChild('Getargv.as_string errors > Throws if the arguments cannot be encoded as requested');
+    try {
+      expect(() => Getargv.as_string(child.pid!, "utf-16", undefined, undefined, { fatal: true })).toThrow({
+        name: 'TypeError',
+        message: "The encoded data was not valid UTF-16LE data",
+      });
+    } finally {
+      child.kill();
+    }
+  });
 
 });
 
 describe("Getargv.as_array errors", () => {
-  test('Requires the "pid" argument be specified', () =>
+  it('Requires the "pid" argument be specified', () =>
     expect(Getargv.as_array).toThrow({
       name: 'TypeError',
       message: 'The "pid" argument must be specified',
     })
   );
 
-  test('Requires the "encoding" argument be specified', () =>
-    expect(() => Getargv.as_array(process.pid)).toThrow({
+  it('Requires the "encoding" argument be specified', () =>
+    expect(() => Getargv.as_array(pid)).toThrow({
       name: 'TypeError',
       message: 'The "encoding" argument must be specified',
     })
   );
 
-  test('Requires the "pid" argument be a number', () =>
+  it('Requires the "pid" argument be a number', () =>
     expect(() => Getargv.as_array('a', 'utf-8')).toThrow({
       name: 'TypeError',
       message: 'The "pid" argument must be a number',
     })
   );
 
-  test('Requires the "encoding" argument be valid', () =>
-    expect(() => Getargv.as_array(process.pid, 'a')).toThrow({
+  it('Requires the "encoding" argument be valid', () =>
+    expect(() => Getargv.as_array(pid, 'a')).toThrow({
       name: 'RangeError',
       message: 'Unsupported encoding label "a"',
     })
   );
 
-  test('Throws if the arguments cannot be encoded as requested', () =>
-    expect(() => Getargv.as_array(process.pid, "utf-16", { fatal: true })).toThrow({
-      name: 'TypeError',
-      message: "The encoded data was not valid UTF-16LE data",
-    })
-  );
+  it('Throws if the arguments cannot be encoded as requested', () => {
+    const child = makeChild('Getargv.as_array errors > Throws if the arguments cannot be encoded as requested');
+    try {
+      expect(() => Getargv.as_array(child.pid!, "utf-16", { fatal: true })).toThrow({
+        name: 'TypeError',
+        message: "The encoded data was not valid UTF-16LE data",
+      });
+    } finally {
+      child.kill();
+    }
+  });
 
 });
 
 describe("Getargv.get_argv_of_pid errors", () => {
-  test('Requires the "pid" argument be specified', () =>
+  it('Requires the "pid" argument be specified', () =>
     expect(Getargv.get_argv_of_pid).toThrow({
       name: 'TypeError',
-      message: 'The "pid" argument must be specified',
+      message: "Invalid number was passed as first argument",
     })
   );
 
-  test('Requires the "pid" argument be a number', () =>
+  it('Requires the "pid" argument be a number', () =>
     expect(() => Getargv.get_argv_of_pid('a')).toThrow({
       name: 'TypeError',
       message: 'Invalid number was passed as first argument',
     })
   );
 
-  test('Requires the "pid" argument be a positive number', () =>
+  it('Requires the "pid" argument be a positive number', () =>
     expect(() => Getargv.get_argv_of_pid(-1)).toThrow({
       name: 'RangeError',
       message: 'Invalid PID was passed as first argument',
     })
   );
 
-  test('Requires the "nulls" argument be a boolean', () =>
-    expect(() => Getargv.get_argv_of_pid(process.pid, 'a')).toThrow({
+  it('Requires the "nulls" argument be a boolean', () =>
+    expect(() => Getargv.get_argv_of_pid(pid, 'a')).toThrow({
       name: 'TypeError',
       message: 'Invalid bool was passed as second argument',
     })
   );
 
-  test('Requires the "skip" argument be a number', () =>
-    expect(() => Getargv.get_argv_of_pid(process.pid, false, 'a')).toThrow({
+  it('Requires the "skip" argument be a number', () =>
+    expect(() => Getargv.get_argv_of_pid(pid, false, 'a')).toThrow({
       name: 'TypeError',
       message: 'Invalid number was passed as third argument',
     })
   );
 
-  test('Requires the "skip" argument be a positive number', () =>
-    expect(() => Getargv.get_argv_of_pid(process.pid, false, -1)).toThrow({
+  it('Requires the "skip" argument be a positive number', () =>
+    expect(() => Getargv.get_argv_of_pid(pid, false, -1)).toThrow({
       name: 'RangeError',
       message: 'Invalid number was passed as third argument, must be between 0 and (1024 * 1024)',
     })
   );
 
-  test('Requires the "skip" argument be a number not greater than ARG_MAX', () =>
-    expect(() => Getargv.get_argv_of_pid(process.pid, false, Getargv.ARG_MAX + 1)).toThrow({
+  it('Requires the "skip" argument be a number not greater than ARG_MAX', () =>
+    expect(() => Getargv.get_argv_of_pid(pid, false, Getargv.ARG_MAX + 1)).toThrow({
       name: 'RangeError',
       message: 'Invalid number was passed as third argument, must be between 0 and (1024 * 1024)',
     })
@@ -171,28 +165,28 @@ describe("Getargv.get_argv_of_pid errors", () => {
 });
 
 describe("Getargv.get_argv_and_argc_of_pid errors", () => {
-  test('Requires the "pid" argument be specified', () =>
+  it('Requires the "pid" argument be specified', () =>
     expect(Getargv.get_argv_and_argc_of_pid).toThrow({
       name: 'TypeError',
       message: 'The "pid" argument must be specified',
     })
   );
 
-  test('Requires that extra arguments not be specified', () =>
-    expect(() => Getargv.get_argv_and_argc_of_pid(process.pid, 'a')).toThrow({
+  it('Requires that extra arguments not be specified', () =>
+    expect(() => Getargv.get_argv_and_argc_of_pid(pid, 'a')).toThrow({
       name: 'TypeError',
       message: 'Too many arguments were provided, max: 1',
     })
   );
 
-  test('Requires the "pid" argument be a positive number', () =>
+  it('Requires the "pid" argument be a positive number', () =>
     expect(() => Getargv.get_argv_and_argc_of_pid(-1)).toThrow({
       name: 'RangeError',
       message: 'Invalid PID was passed as first argument',
     })
   );
 
-  test('Requires the "pid" argument be a number not greater than PID_MAX', () =>
+  it('Requires the "pid" argument be a number not greater than PID_MAX', () =>
     expect(() => Getargv.get_argv_and_argc_of_pid(1 + Getargv.PID_MAX)).toThrow({
       name: 'RangeError',
       message: 'Invalid PID was passed as first argument',
@@ -202,47 +196,46 @@ describe("Getargv.get_argv_and_argc_of_pid errors", () => {
 
 describe("Correct functionality", () => {
 
-  test('Getargv.get_argv_and_argc_of_pid returns correct output', () => {
-    const result = Getargv.get_argv_and_argc_of_pid(process.pid);
-    const expected = expected_args();
+  it('Getargv.get_argv_and_argc_of_pid returns correct output', () => {
+    const result = Getargv.get_argv_and_argc_of_pid(pid);
+    const expected = expectedArgs();
     expect(result).toStrictEqual(expected);
   });
 
-  test('Getargv.get_argv_of_pid returns correct output', () => {
-    const enc = new TextEncoder();
-    const dec = new TextDecoder();
-    const result = Getargv.get_argv_of_pid(process.pid);
-    const expected_string = expected_args().map(e => dec.decode(e)).join("");
-    expect(dec.decode(result)).toStrictEqual(expected_string);
-    expect(result).toStrictEqual(enc.encode(expected_string).buffer);
+  it('Getargv.get_argv_of_pid returns correct output', () => {
+    const result = Getargv.get_argv_of_pid(pid);
+    const expected_string = expectedString();
+    expect(fromBuffer(result)).toStrictEqual(expected_string);
+    expect(result).toStrictEqual(toBuffer(expected_string));
   });
 
-  test('Getargv.as_string returns correct utf8 output', () => {
-    const dec = new TextDecoder();
-    const result = Getargv.as_string(process.pid, "utf-8");
-    const expected = expected_args().map(e => dec.decode(e)).join("");
+  it('Getargv.as_string returns correct utf8 output', () => {
+    const result = Getargv.as_string(pid, "utf-8");
+    const expected = expectedString("utf-8");
     expect(result).toBe(expected);
   });
 
-  test('Getargv.as_array returns correct utf8 output', () => {
-    const dec = new TextDecoder();
-    const result = Getargv.as_array(process.pid, "utf-8");
-    const expected = expected_args().map(e => dec.decode(e));
+  it('Getargv.as_array returns correct utf8 output', () => {
+    const result = Getargv.as_array(pid, "utf-8");
+    const expected = expectedArray("utf-8");
     expect(result).toBe(expected);
   });
 
-  test('Getargv.as_string returns correct utf16 output', () => {
-    const dec = new TextDecoder();
-    const result = Getargv.as_string(process.pid, "utf-16");
-    const expected = expected_args().map(e => dec.decode(e)).join("");
-    expect(result).not.toBe(expected);
+  it('Getargv.as_string returns correct utf16 output', () => {
+    const child = makeChild('Correct functionality > Getargv.as_string returns correct utf16 output', false);
+    try {
+      const result = Getargv.as_string(child.pid!, "utf-16");
+      const expected = fromBuffer(child.spawnargs.map(toBuffer).reduce(appendBuffers), "utf-16");
+      expect(result).toBe(expected);
+    } finally {
+      child.kill();
+    }
   });
 
-  test('Getargv.as_array returns correct utf16 output', () => {
-    const dec = new TextDecoder();
-    const result = Getargv.as_array(process.pid, "utf-16");
-    const expected = expected_args().map(e => dec.decode(e));
-    expect(result).not.toBe(expected);
+  it('Getargv.as_array returns correct utf16 output', () => {
+    const result = Getargv.as_array(pid, "utf-16");
+    const expected = expectedArray("utf-16");
+    expect(result).toStrictEqual(expected);
   });
 
 });
